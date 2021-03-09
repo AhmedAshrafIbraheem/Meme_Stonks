724f4bf225d0483d09367e5975dea4bcedbbd2c9
Shourav <shouravahmed25@gmail.com>
2021-03-08 21:15:34 -0500

    first commit

--- Data_Collecting/market_stock_info.py
+++ Data_Collecting/market_stock_info.py
@@ -1,3 +1,12 @@
+import requests
+import json
 
 def get_market_stock_info(ticker):
-    pass
+    url = f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={ticker}&apikey=75NGNLOY2P6P3HT4'
+    response = requests.get(url)
+    info = response.json()
+    print(info['Symbol'])
+    print(info['Name'])
+    print(info['Description'])
+
+get_market_stock_info('AAPL')
