5d535dd07623fa14133856cb7211ed21e93ff12c
maimedlord <52831314+maimedlord@users.noreply.github.com>
2021-03-30 19:52:26 -0400

    MAJOR CHANGES
    added in a helper function for both google_trends and google_trends_normalized called averager
    
    added in new function google_trends_normalized that returns a nested dictionary containing data that can be used to graph trending differences between the top 10 stocks.
    
    I will be updating comments for this code shortly but wanted to get this commit in so that it can be viewed for tomorrow (Wednesday)

--- Data_Collecting/social_stock_info.py
+++ Data_Collecting/social_stock_info.py
@@ -1,21 +1,144 @@
-from requests import get
-import tweepy
-import re
+# social_stock_info.py
+# slamstonks.com
+# ...
+# ...
+#from requests import get
+#import tweepy
+#import re
 import pandas as pd
 from pytrends.request import TrendReq
+
 
 # THE CODE TO DO THIS IS NOT DONE YET!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # pull top_10 stocks from db or from the controller that calls this and the mw scraper:
 top_10_stocks = ["LAZR", "KMPH", "TMBR", "EYES", "FBRX", "ROOT", "OTRK", "HJLI", "PUBM", "ASO"]
 
+############################################################
+#
+### AVERAGER ###############################################
+# Accepts a ticker dictionary that has hours per day and a
+# corresponding value. These hour values are averaged for
+# the day and returned in a dictionary.
+def averager(ticker_dictionary):
+    dictionary_to_return = {}  # loads nested_dictionary_return when full and empties for next ticker
+    popping_from_ticker = ticker_dictionary.popitem()  # pops a tuple from into the var
+    current_date = popping_from_ticker[0].strftime("%Y-%m-%d")  # string
+    counter = popping_from_ticker[1]  # int
+    counter_for_avg = 1
+    while (len(ticker_dictionary) > 0):
+        popping_from_ticker = ticker_dictionary.popitem()
+        temp_current_date = popping_from_ticker[0].strftime("%Y-%m-%d")
+        if temp_current_date == current_date:
+            counter_for_avg += 1
+            counter += popping_from_ticker[1]
+        if temp_current_date != current_date:
+            dictionary_to_return.update({current_date: counter / counter_for_avg})
+            current_date = temp_current_date
+            counter = popping_from_ticker[1]
+            counter_for_avg = 1
+        if len(ticker_dictionary) == 0:
+            dictionary_to_return.update({current_date: counter / counter_for_avg})
+
+    # print(dictionary_to_return)
+
+    return dictionary_to_return
+# END averager
+
 
 ############################################################
-# should be run once every 10 minutes (same time as marketwatch scraper)...
-### GOOGLE TRENDS ##########################################
+# ......................................................................................................................
+### GOOGLE TRENDS ALL ######################################
 # pytrends, pandas (dataframe, timeframe)
-# returns a nested dictionary containing tickers and their
-# individual trending percentages for the last seven days
-# (including today)
+# returns a nested dictonary containing data needed for
+# comparing the relative frequency of tickers compared to
+# each other.
+# Very helpful: https://towardsdatascience.com/telling-stories-with-google-trends-using-pytrends-in-python-a11e5b8a177
+def google_trends_normalized(top_10_stocks):
+    # reference keyword is the last element in top_10_stocks
+    first_three_tickers = top_10_stocks[0:3]
+    first_three_tickers.append(top_10_stocks[9])
+    second_three_tickers = top_10_stocks[3:6]
+    second_three_tickers.append(top_10_stocks[9])
+    last_four_tickers = top_10_stocks[6:10]
+
+    # Only need to load this one time for following operations:
+    pytrends = TrendReq()
+    # pull 1
+    kw_list = first_three_tickers
+    pytrends.build_payload(kw_list, cat=7, timeframe='now 7-d', geo='US', gprop='')
+    interest_over_time_1_df = pytrends.interest_over_time()
+    interest_over_time_1_df.rename(columns={top_10_stocks[9]: top_10_stocks[9] + '1'}, inplace=True)
+    del interest_over_time_1_df['isPartial']
+    #print(interest_over_time_1_df.head())        # useful for error testing
+    # pull 2
+    kw_list = second_three_tickers
+    pytrends.build_payload(kw_list, cat=7, timeframe='now 7-d', geo='US', gprop='')
+    interest_over_time_2_df = pytrends.interest_over_time()
+    interest_over_time_2_df.rename(columns={top_10_stocks[9]: top_10_stocks[9] + '2'}, inplace=True)
+    del interest_over_time_2_df['isPartial']
+    #print(interest_over_time_2_df.head())      # useful for error testing
+    # pull 3
+    kw_list = last_four_tickers
+    pytrends.build_payload(kw_list, cat=7, timeframe='now 7-d', geo='US', gprop='')
+    interest_over_time_3_df = pytrends.interest_over_time()
+    interest_over_time_3_df.rename(columns={top_10_stocks[9]: top_10_stocks[9] + '3'}, inplace=True)
+    del interest_over_time_3_df['isPartial']
+    #print(interest_over_time_3_df.head())     # useful for error testing
+
+    master_df = pd.concat([interest_over_time_1_df.reset_index(), interest_over_time_2_df.reset_index(), interest_over_time_3_df.reset_index()], axis=1)
+    master_df = master_df.loc[:,~master_df.columns.duplicated()]
+    master_df.set_index('date', inplace=True)
+    #print(master_df.to_string())
+
+    nested_dictionary = {}
+
+    tickers_plus_normalizers_list = list(master_df.columns)
+
+    #print(tickers_plus_normalizers_list)
+
+    for x in tickers_plus_normalizers_list:
+        # prepping variables for the coming while loop that uses them. We need to get the current_date before we can
+        # cycle through the dictionary. current_date is critical to successful execution.
+        ticker_only_dictionary = master_df.to_dict().get(x) # grabs the ticker dictionary from the dataframe
+        #print(ticker_only_dictionary)
+        nested_dictionary.update({x: averager(ticker_only_dictionary)})
+
+    #print(nested_dictionary)
+
+    for_loop_counter = 0
+    for x in tickers_plus_normalizers_list:
+        #print(x)
+        if for_loop_counter < 3:
+            for xx in nested_dictionary[x]:
+                nested_dictionary[x][xx] = round((float(nested_dictionary[x][xx]) + .01) * (float(nested_dictionary[tickers_plus_normalizers_list[7]][xx]) + .01) / (float(nested_dictionary[tickers_plus_normalizers_list[3]][xx]) + .01), 2)
+                #print(x, xx, nested_dictionary[x][xx], sep="\t\t\t")
+        if for_loop_counter == 3:
+            for xx in nested_dictionary[x]:
+                nested_dictionary[x][xx] = round(nested_dictionary[x][xx], 2)
+                #print(x, xx, nested_dictionary[x][xx], sep="\t\t\t")
+        if for_loop_counter > 3 and for_loop_counter < 7:
+            for xx in nested_dictionary[x]:
+                nested_dictionary[x][xx] = round((float(nested_dictionary[x][xx]) + .01) * (float(nested_dictionary[tickers_plus_normalizers_list[3]][xx]) + .01) / (float(nested_dictionary[tickers_plus_normalizers_list[7]][xx]) + .01), 2)
+                #print(x, xx, nested_dictionary[x][xx], sep="\t\t\t")
+        #if for_loop_counter == 7:
+            #for xx in nested_dictionary[x]:
+                #print(x, xx, nested_dictionary[x][xx], sep="\t\t\t")
+        if for_loop_counter > 7 and for_loop_counter < 11:
+            for xx in nested_dictionary[x]:
+                nested_dictionary[x][xx] = round((float(nested_dictionary[x][xx]) + .01) * (float(nested_dictionary[tickers_plus_normalizers_list[3]][xx]) + .01) / (float(nested_dictionary[tickers_plus_normalizers_list[11]][xx]) + .01), 2)
+                #print(x, xx, nested_dictionary[x][xx], sep="\t\t\t")
+
+        for_loop_counter += 1
+
+    nested_dictionary.pop(tickers_plus_normalizers_list[7])
+    nested_dictionary.pop(tickers_plus_normalizers_list[11])
+
+    #print(nested_dictionary)
+
+    return nested_dictionary
+# END google_trends_normalized
+
+
 def google_trends(ticker):
     # Only need to load this one time for following operations:
     pytrends = TrendReq()
@@ -35,31 +158,8 @@ def google_trends(ticker):
     # prepping variables for the coming while loop that uses them. We need to get the current_date before we can
     # cycle through the dictionary. current_date is critical to successful execution.
     ticker_only_dictionary = interest_over_time_df.to_dict().get(ticker) # grabs the ticker dictionary from the dataframe
-    # ###
     # print(interest_over_time_df.to_string())
-    # ###
-    dictionary_to_return = {}        # loads nested_dictionary_return when full and empties for next ticker
-    popping_from_ticker = ticker_only_dictionary.popitem()      # pops a tuple from into the var
-    current_date = popping_from_ticker[0].strftime("%Y-%m-%d")      # string
-    counter = popping_from_ticker[1]        # int
-    counter_for_avg = 1
-    while(len(ticker_only_dictionary) > 0):
-        popping_from_ticker = ticker_only_dictionary.popitem()
-        temp_current_date = popping_from_ticker[0].strftime("%Y-%m-%d")
-        if temp_current_date == current_date:
-            counter_for_avg += 1
-            counter += popping_from_ticker[1]
-        if temp_current_date != current_date:
-            dictionary_to_return.update({current_date: counter / counter_for_avg})
-            current_date = temp_current_date
-            counter = popping_from_ticker[1]
-            counter_for_avg = 1
-        if len(ticker_only_dictionary) == 0:
-            dictionary_to_return.update({current_date: counter / counter_for_avg})
-
-    # print(dictionary_to_return)
-
-    return dictionary_to_return
+    return averager(ticker_only_dictionary)
 # END google_trends
 
 
@@ -122,8 +222,8 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print(google_trends("LAZR"))
+#if __name__ == '__main__':
+    #print(google_trends_normalized(top_10_stocks))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 # END social_stock_info.py
